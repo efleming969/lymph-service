79f287117bdde0bba286fa6412f6f2e270802732
Erick Fleming <efleming969@gmail.com>
2018-03-03 10:33:55 -0600

    added http wrapper for native http module

--- src/HTTP.tests.ts
+++ src/HTTP.tests.ts
@@ -1,3 +1,30 @@
-test( "sanity test", function () {
-    expect( true ).toBeTruthy()
+import HTTP from "./HTTP"
+
+test( "getting json data from remote service", function () {
+    const http = new HTTP( "https://httpbin.org" )
+
+    return http.get( "/get?name=foobar" ).then( function ( data ) {
+        expect( data.args ).toEqual( {
+            name: "foobar"
+        } )
+    } )
+} )
+
+test( "posting json data to remote service", function () {
+    const http = new HTTP( "https://httpbin.org" )
+
+    return http.post( "/post", { name: "foobar" } ).then( function ( response ) {
+        expect( response.status ).toEqual( 200 )
+        expect( response.body.json ).toEqual( {
+            name: "foobar"
+        } )
+    } )
+} )
+
+test( "handling an error when posting to invalid uri", function () {
+    const http = new HTTP( "https://httpbin.org" )
+
+    return http.post( "/invalid", {} ).then( function ( response ) {
+        expect( response.status ).toEqual( 404 )
+    } )
 } )
---
+++ src/HTTP.ts
@@ -0,0 +1,97 @@
+import * as NativeHTTP from "https"
+import * as URL from "url"
+
+export default class HTTP {
+    protocol: string
+    hostname: string
+    port: string
+
+    constructor ( baseUrl: string ) {
+        const baseUrlParts = URL.parse( baseUrl )
+        this.protocol = baseUrlParts.protocol
+        this.hostname = baseUrlParts.hostname
+        this.port = baseUrlParts.port
+    }
+
+    get ( path: string ): Promise<any> {
+        return new Promise( ( resolve, reject ) => {
+
+            const options = {
+                protocol: this.protocol
+                , hostname: this.hostname
+                , port: this.port
+                , path: path
+                , method: "GET"
+            }
+
+            const req = NativeHTTP.request( options, function ( res ) {
+                let data: string[] = []
+
+                res.setEncoding( "utf8" )
+
+                res.on( "data", function ( chunk: string ) {
+                    data.push( chunk )
+                } )
+
+                res.on( "end", function () {
+                    resolve( JSON.parse( data.join( "" ) ) )
+                } )
+            } )
+
+            req.on( "error", function ( err ) {
+                reject( err )
+            } )
+
+            req.end()
+        } )
+    }
+
+    post ( path: string, data: any ): Promise<any> {
+        const serialized_data = JSON.stringify( data )
+
+        return new Promise( ( resolve, reject ) => {
+            const options = {
+                protocol: this.protocol,
+                hostname: this.hostname,
+                port: this.port,
+                path: path,
+                method: "POST",
+                headers: {
+                    "Content-type": "application/json",
+                    "Content-length": serialized_data.length
+                }
+            }
+
+            const req = NativeHTTP.request( options, function ( res ) {
+                let response_data: string[] = []
+
+                res.setEncoding( "utf8" )
+
+                res.on( "data", function ( chunk: string ) {
+                    response_data.push( chunk )
+                } )
+
+                res.on( "end", function () {
+                    let body = undefined
+
+                    try {
+                        body = JSON.parse( response_data.join( "" ) )
+                    } catch ( e ) {
+                    }
+                    resolve( {
+                        status: res.statusCode,
+                        headers: res.headers,
+                        body: body
+                    } )
+                } )
+            } )
+
+            req.on( "error", function ( error ) {
+                reject( error )
+            } )
+
+            req.write( serialized_data )
+            req.end()
+        } )
+    }
+}
